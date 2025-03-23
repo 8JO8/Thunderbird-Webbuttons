304f38ef6ecf2fe9a7e23b5cfb1e2d23e363ab0a
8JO8 <76175269+8JO8@users.noreply.github.com>
2025-03-23 18:15:24 +0100

    Updated User Agent

--- skype/redirect.js
+++ skype/redirect.js
@@ -8,7 +8,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(
   function(e) {
     e.requestHeaders.forEach(header => {
       if (header.name.toLowerCase() === "user-agent") {
-        header.value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/122.0";
+        header.value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0";
       }
     });
     return { requestHeaders: e.requestHeaders };
